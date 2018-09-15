716d7d1d78389b25843fdb4ec269b20c96c1d71e
Artur Leśniewski <kontakt@drzewakodow.pl>
2018-09-15 13:42:36 +0200

    [#2.4] - dodanie komponentu addProductForm.

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -4,27 +4,18 @@
             <li class="list-item" v-for="p in products" :key="p.id">{{ p.name }}</li>
         </ul>
 
+        <add-product-form v-bind:products="products"></add-product-form>
+
         <p v-if="!products.length">No products!</p>
 
-        <form @submit.prevent="onSubmit()">
-            <input
-                    name="product"
-                    v-model="newProduct.name"
-                    v-validate="'required|min:3'"
-            >
-            <button>Add</button>
-            <!--3- 2. errors are added by default when validation is initialized and have some useful methods -->
-            <div v-show="errors.has('product')">
-                {{ errors.first('product') }}
-            </div>
-        </form>
     </div>
 </template>
 
 <script>
-    import uuid from 'uuid/v4';
+    import AddProductForm from "./productAddForm.vue";
 
     export default {
+        components: {AddProductForm},
         name: 'ProductList',
         data() {
             return {
@@ -38,23 +29,6 @@
                 newProduct: {
                     name: ''
                 }
-            }
-        },
-        methods: {
-            onSubmit() {
-                // 3. On the JS side we need to use yet another injected value called $validator to validate all the fields
-                this.$validator.validateAll().then(result => {
-                    if (!result) {
-                        return;
-                    }
-                    this.products.push({
-                        id: uuid(),
-                        ...this.newProduct
-                    });
-                    this.newProduct.name = '';
-                    // 4/ and reset validation state after adding a product
-                    this.$validator.reset();
-                });
             }
         }
     }
