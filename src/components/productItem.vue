2b7185426aae99b3e9c6b9f8d42f6c543135d7c0
Artur Leśniewski <kontakt@drzewakodow.pl>
2018-09-15 15:08:11 +0200

    [#3.1][#3.2][#3.3] - sotowanie i wydzielenie nowych komponentów.

---
+++ src/components/productItem.vue
@@ -0,0 +1,52 @@
+<template>
+    <li class="list-item">
+        {{ name }}
+        <button class="btn btn-danger" v-on:click="removeItem(id)">Remove</button>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: 'ProductItem',
+        props: {
+            name: String,
+            id: String
+        },
+        methods: {
+            removeItem(key) {
+                this.$emit('removeItem', key);
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .list-item {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        transition: all 0.5s;
+    }
+
+    .v-enter,
+    .v-leave-to
+        /* .card-leave-active for <2.1.8 */ {
+        opacity: 0;
+        transform: scale(0);
+    }
+
+    .v-enter-to {
+        opacity: 1;
+        transform: scale(1);
+    }
+
+    .v-leave-active {
+        /*position: absolute;*/
+    }
+
+    .v-move {
+        opacity: 1;
+        transition: all 0.5s;
+    }
+</style>
---
+++ src/components/sortBtnList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <button @click="sort" class="btn btn-sm btn-block btn-default" type="button">Sort list</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SortBtnList',
+        props: {
+            products: Array
+        },
+        methods: {
+            sort(){
+                this.$emit('sortItems');
+            }
+        },
+    };
+</script>
+<style scoped>
+
+</style>
